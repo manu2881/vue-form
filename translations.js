0d94a586ae9fb69d1a97ca6357e89e4254b70835
manu2881 <65648759+manu2881@users.noreply.github.com>
2020-05-20 13:27:30 +0530

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,32 @@
+export default {
+  en: {
+    form: {
+      fullName: "Full name",
+      age: "Age",
+      email: "Email",
+      terms: "I accept the terms and conditions",
+      gender: "Select gender",
+      country: "Select country",
+      additionalInfo: "Additional info",
+      submitted: "The form is submitted!",
+      sentInfo: "Here is the info you sent:",
+      return: "Return to the form",
+      submit: "Submit",
+      submitting: "Submitting",
+      charactersLeft: "You have {charCount} character left. | You have {charCount} characters left.",
+      genders: {
+        Male: "Male",
+        Female: "Female",
+        Others: "others"
+      }
+    },
+    error: {
+      invalidFields: "Following fields have an invalid or a missing value:",
+      general: "An error happened during submit.",
+      generalMessage: "Form sending failed due to technical problems. Try again later.",
+      fieldRequired: "{field} is required.",
+      fieldInvalid: "{field} is invalid or missing.",
+      fieldMaxLength: "{field} maximum characters exceeded."
+    }
+  }
+};
